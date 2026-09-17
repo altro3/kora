9a47cfcfe94069646bc9a9d7b76fc47b96091467
Altro <alexuyt@gmail.com>
2026-09-17 18:16:56 +0700

    build: introduce kora-publishing convention plugin in build-logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     id("io.koraframework.kora-publishing")
     id("io.koraframework.kora-maven-publishing")
+    id("io.koraframework.kora-ci-testing")
     jacoco
 }
