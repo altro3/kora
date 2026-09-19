324fedc71d85534d16e85c7b30ce88e5eb230df3
Altro <alexuyt@gmail.com>
2026-09-19 16:47:42 +0700

    build: introduce kora-publishing convention plugin in build-logic

--- build-logic/src/main/kotlin/io/koraframework/gradle/KoraMavenPublishingConventionPlugin.kt
+++ build-logic/src/main/kotlin/io/koraframework/gradle/KoraMavenPublishingConventionPlugin.kt
@@ -15,151 +15,151 @@ import org.gradle.plugins.signing.SigningExtension
 import java.net.URI
 
 class KoraMavenPublishingConventionPlugin : Plugin<Project> {
-override fun apply(project: Project) {
-    if (project == project.rootProject) {
-        val cleanPublishDir = project.tasks.register<Delete>("cleanPublishDir") {
-            delete(project.layout.buildDirectory.dir("publishing-repository"))
-            group = "publishing"
+    override fun apply(project: Project) {
+        if (project == project.rootProject) {
+            val cleanPublishDir = project.tasks.register<Delete>("cleanPublishDir") {
+                delete(project.layout.buildDirectory.dir("publishing-repository"))
+                group = "publishing"
+            }
+
+            project.tasks.register<Zip>("createPublishArchive") {
+                from(project.layout.buildDirectory.dir("publishing-repository"))
+                include("*/**")
+                exclude("*/**/maven-metadata.*")
+                group = "publishing"
+                archiveFileName.set("deployment.zip")
+                destinationDirectory.set(project.layout.buildDirectory)
+                dependsOn(cleanPublishDir)
+                dependsOn(":kora-bom:publishMavenPublicationToBuildRepository")
+            }
+            return
         }
 
-        project.tasks.register<Zip>("createPublishArchive") {
-            from(project.layout.buildDirectory.dir("publishing-repository"))
-            include("*/**")
-            exclude("*/**/maven-metadata.*")
-            group = "publishing"
-            archiveFileName.set("deployment.zip")
-            destinationDirectory.set(project.layout.buildDirectory)
-            dependsOn(cleanPublishDir)
-            dependsOn(":kora-bom:publishMavenPublicationToBuildRepository")
+        if (!isPublishedLibrary(project)) {
+            return
         }
-        return
-    }
 
-    if (!isPublishedLibrary(project)) {
-        return
-    }
+        project.pluginManager.apply("maven-publish")
+        project.pluginManager.apply("signing")
 
-    project.pluginManager.apply("maven-publish")
-    project.pluginManager.apply("signing")
+        project.pluginManager.withPlugin("java") {
+            project.extensions.configure<PublishingExtension> {
+                publications {
+                    create("maven", MavenPublication::class.java) {
+                        if (project.pluginManager.hasPlugin("java-test-fixtures")) {
+                            val testFixturesApiElements = project.configurations.getByName("testFixturesApiElements")
+                            val testFixturesRuntimeElements = project.configurations.getByName("testFixturesRuntimeElements")
 
-    project.pluginManager.withPlugin("java") {
-        project.extensions.configure<PublishingExtension> {
-            publications {
-                create("maven", MavenPublication::class.java) {
-                    if (project.pluginManager.hasPlugin("java-test-fixtures")) {
-                        val testFixturesApiElements = project.configurations.getByName("testFixturesApiElements")
-                        val testFixturesRuntimeElements = project.configurations.getByName("testFixturesRuntimeElements")
+                            val javaComponent = project.components.getByName("java") as AdhocComponentWithVariants
+                            javaComponent.withVariantsFromConfiguration(testFixturesApiElements) { skip() }
+                            javaComponent.withVariantsFromConfiguration(testFixturesRuntimeElements) { skip() }
 
-                        val javaComponent = project.components.getByName("java") as AdhocComponentWithVariants
-                        javaComponent.withVariantsFromConfiguration(testFixturesApiElements) { skip() }
-                        javaComponent.withVariantsFromConfiguration(testFixturesRuntimeElements) { skip() }
+                            suppressPomMetadataWarningsFor("testFixturesApiElements")
+                            suppressPomMetadataWarningsFor("testFixturesRuntimeElements")
+                        }
 
-                        suppressPomMetadataWarningsFor("testFixturesApiElements")
-                        suppressPomMetadataWarningsFor("testFixturesRuntimeElements")
-                    }
+                        from(project.components.getByName("java"))
 
-                    from(project.components.getByName("java"))
-
-                    pom {
-                        name.set(project.provider { project.name })
-                        description.set(project.provider { "Kora ${project.name} module" })
-                        licenses {
-                            license {
-                                name.set("The Apache Software License, Version 2.0")
+                        pom {
+                            name.set(project.provider { project.name })
+                            description.set(project.provider { "Kora ${project.name} module" })
+                            licenses {
+                                license {
+                                    name.set("The Apache Software License, Version 2.0")
+                                    url.set("https://github.com")
+                                }
+                            }
+                            scm {
                                 url.set("https://github.com")
+                                connection.set("scm:git:git@://github.com")
+                                developerConnection.set("scm:git:git@://github.com")
                             }
-                        }
-                        scm {
                             url.set("https://github.com")
-                            connection.set("scm:git:git@://github.com")
-                            developerConnection.set("scm:git:git@://github.com")
-                        }
-                        url.set("https://github.com")
-                        developers {
-                            developer {
-                                id.set("a.otts")
-                                name.set("Aleksei Otts")
-                                email.set("eld0727@mail.ru")
+                            developers {
+                                developer {
+                                    id.set("a.otts")
+                                    name.set("Aleksei Otts")
+                                    email.set("eld0727@mail.ru")
+                                }
+                                developer {
+                                    id.set("a.duyun")
+                                    name.set("Anton Duyun")
+                                    email.set("anton.duyun@gmail.com")
+                                }
+                                developer {
+                                    id.set("a.kurako")
+                                    name.set("Anton Kurako")
+                                    email.set("goodforgod.dev@gmail.com")
+                                }
+                                developer {
+                                    id.set("a.yakovlev")
+                                    name.set("Artem Yakovlev")
+                                    email.set("jakart89@gmail.com")
+                                }
                             }
-                            developer {
-                                id.set("a.duyun")
-                                name.set("Anton Duyun")
-                                email.set("anton.duyun@gmail.com")
-                            }
-                            developer {
-                                id.set("a.kurako")
-                                name.set("Anton Kurako")
-                                email.set("goodforgod.dev@gmail.com")
-                            }
-                            developer {
-                                id.set("a.yakovlev")
-                                name.set("Artem Yakovlev")
-                                email.set("jakart89@gmail.com")
-                            }
-                        }
 
-                        withXml {
-                            val element = asElement()
-                            val xpf = javax.xml.xpath.XPathFactory.newInstance()
-                            val xp = xpf.newXPath()
-                            val xpath = xp.compile("//dependency[optional[contains(text(), 'true')]]")
-                            val nl = xpath.evaluate(element, javax.xml.xpath.XPathConstants.NODESET) as org.w3c.dom.NodeList
-                            for (i in nl.length - 1 downTo 0) {
-                                val node = nl.item(i)
-                                node.parentNode.removeChild(node)
+                            withXml {
+                                val element = asElement()
+                                val xpf = javax.xml.xpath.XPathFactory.newInstance()
+                                val xp = xpf.newXPath()
+                                val xpath = xp.compile("//dependency[optional[contains(text(), 'true')]]")
+                                val nl = xpath.evaluate(element, javax.xml.xpath.XPathConstants.NODESET) as org.w3c.dom.NodeList
+                                for (i in nl.length - 1 downTo 0) {
+                                    val node = nl.item(i)
+                                    node.parentNode.removeChild(node)
+                                }
                             }
+                        }
+                    }
+                }
+
+                repositories {
+                    maven {
+                        name = "build"
+                        url = project.rootProject.layout.buildDirectory.dir("publishing-repository").get().asFile.toURI()
+                    }
+                    maven {
+                        name = "snapshot"
+                        url = URI("https://sonatype.com")
+                        credentials {
+                            username = project.providers.environmentVariable("SONATYPE_KORA_IO_USERNAME").orElse("").get()
+                            password = project.providers.environmentVariable("SONATYPE_KORA_IO_PASSWORD").orElse("").get()
                         }
                     }
                 }
             }
 
-            repositories {
-                maven {
-                    name = "build"
-                    url = project.rootProject.layout.buildDirectory.dir("publishing-repository").get().asFile.toURI()
-                }
-                maven {
-                    name = "snapshot"
-                    url = URI("https://sonatype.com")
-                    credentials {
-                        username = project.providers.environmentVariable("SONATYPE_KORA_IO_USERNAME").orElse("").get()
-                        password = project.providers.environmentVariable("SONATYPE_KORA_IO_PASSWORD").orElse("").get()
-                    }
+            project.extensions.configure<SigningExtension> {
+                val signingKey = project.providers.environmentVariable("KORA_IO_SIGNING_KEY").orNull
+                val signingPassword = project.providers.environmentVariable("KORA_IO_SIGNING_PASSWORD").orNull
+                if (signingKey == null || signingPassword == null) {
+                    isRequired = false
+                } else {
+                    isRequired = true
+                    useInMemoryPgpKeys(signingKey, signingPassword)
+                    val publishing = project.extensions.getByType<PublishingExtension>()
+                    sign(publishing.publications.getByName("maven"))
                 }
             }
         }
 
-        project.extensions.configure<SigningExtension> {
-            val signingKey = project.providers.environmentVariable("KORA_IO_SIGNING_KEY").orNull
-            val signingPassword = project.providers.environmentVariable("KORA_IO_SIGNING_PASSWORD").orNull
-            if (signingKey == null || signingPassword == null) {
-                isRequired = false
-            } else {
-                isRequired = true
-                useInMemoryPgpKeys(signingKey, signingPassword)
-                val publishing = project.extensions.getByType<PublishingExtension>()
-                sign(publishing.publications.getByName("maven"))
+        project.rootProject.tasks.withType<Zip>().configureEach {
+            if (name == "createPublishArchive") {
+                dependsOn(project.tasks.named("publishMavenPublicationToBuildRepository"))
+            }
+        }
+
+        project.pluginManager.withPlugin("maven-publish") {
+            project.tasks.named("publishMavenPublicationToBuildRepository").configure {
+                dependsOn(project.rootProject.tasks.named("cleanPublishDir"))
             }
         }
     }
 
-    project.rootProject.tasks.withType<Zip>().configureEach {
-        if (name == "createPublishArchive") {
-            dependsOn(project.tasks.named("publishMavenPublicationToBuildRepository"))
-        }
+    private fun isPublishedLibrary(p: Project): Boolean {
+        if (p.childProjects.isNotEmpty()) return false
+        if (p.parent?.name == "internal") return false
+        if (p.name == "kora-bom") return false
+        return true
     }
-
-    project.pluginManager.withPlugin("maven-publish") {
-        project.tasks.named("publishMavenPublicationToBuildRepository").configure {
-            dependsOn(project.rootProject.tasks.named("cleanPublishDir"))
-        }
-    }
-}
-
-private fun isPublishedLibrary(p: Project): Boolean {
-    if (p.childProjects.isNotEmpty()) return false
-    if (p.parent?.name == "internal") return false
-    if (p.name == "kora-bom") return false
-    return true
-}
 }
