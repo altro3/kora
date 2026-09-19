007cadfce5dcf60cae62d8c0bbf0a9208bad7c2e
Altro <alexuyt@gmail.com>
2026-09-19 20:42:26 +0700

    build: introduce kora-publishing convention plugin in build-logic

--- build-logic/src/main/kotlin/io/koraframework/gradle/test/KoraTestConventionPlugin.kt
+++ build-logic/src/main/kotlin/io/koraframework/gradle/test/KoraTestConventionPlugin.kt
@@ -4,6 +4,9 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.api.tasks.testing.Test
+import org.gradle.api.tasks.testing.TestDescriptor
+import org.gradle.api.tasks.testing.TestResult
+import org.gradle.api.tasks.testing.TestResult.ResultType
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.gradle.kotlin.dsl.getByType
@@ -34,6 +37,19 @@ class KoraTestConventionPlugin : Plugin<Project> {
             project.tasks.withType<Test>().configureEach {
                 forkEvery = 0
                 failFast = true
+                addTestListener(object : org.gradle.api.tasks.testing.TestListener {
+                    override fun beforeSuite(suite: TestDescriptor) {}
+                    override fun afterSuite(suite: TestDescriptor, result: TestResult) {}
+                    override fun beforeTest(testDescriptor: TestDescriptor) {}
+
+                    override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {
+                        if (result.resultType == ResultType.FAILURE) {
+                            logger.lifecycle("\n!!! [FAIL FAST] Test failed in ${project.path}: ${testDescriptor.className} -> ${testDescriptor.name} !!!")
+                            logger.lifecycle("!!! Killing all parallel workers immediately !!!\n")
+                            Runtime.getRuntime().halt(1)
+                        }
+                    }
+                })
                 failOnNoDiscoveredTests.set(false)
                 environment(System.getenv())
 
